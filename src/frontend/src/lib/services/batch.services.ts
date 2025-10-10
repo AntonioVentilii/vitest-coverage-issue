a815b9b5c50b60ec97779fddba613892392180a7
Antonio Ventilii <antonioventilii@gmail.com>
2025-10-10 22:35:57 +0200

    clean

--- src/frontend/src/lib/services/batch.services.ts
+++ src/frontend/src/lib/services/batch.services.ts
@@ -1,21 +1,4 @@
-import { randomWait } from '$lib/utils/time.utils';
-
-export const batch = async function* <T>({
-	promises,
-	batchSize
-}: {
-	promises: (() => Promise<T>)[];
-	batchSize: number;
-}): AsyncGenerator<PromiseSettledResult<T>[]> {
-	for (let i = 0; i < promises.length; i += batchSize) {
-		const batch = promises.slice(i, i + batchSize);
-		const results = await Promise.allSettled(batch.map((fn) => fn()));
-		yield results;
-		await randomWait({});
-	}
-};
-
-export const createBatches = <T>({ items, batchSize }: { items: T[]; batchSize: number }): T[][] =>
-	Array.from({ length: Math.ceil(items.length / batchSize) }, (_, index) =>
-		items.slice(index * batchSize, (index + 1) * batchSize)
-	);
+export const createBatches = <T>({items, batchSize}: { items: T[]; batchSize: number }): T[][] =>
+    Array.from({length: Math.ceil(items.length / batchSize)}, (_, index) =>
+        items.slice(index * batchSize, (index + 1) * batchSize)
+    );
