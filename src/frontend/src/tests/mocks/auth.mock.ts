f55716f5d554ca49c7a4c3a48b2c8e4846d8c864
Antonio Ventilii <antonioventilii@gmail.com>
2025-10-10 23:47:26 +0200

    clean

--- src/frontend/src/tests/mocks/auth.mock.ts
+++ src/frontend/src/tests/mocks/auth.mock.ts
@@ -1,12 +1,7 @@
 import * as authStore from '$lib/derived/auth.derived';
-import { mockIdentity } from '$tests/mocks/identity.mock';
-import type { Identity } from '@dfinity/agent';
-import { readable } from 'svelte/store';
+import {mockIdentity} from '$tests/mocks/identity.mock';
+import type {Identity} from '@dfinity/agent';
+import {readable} from 'svelte/store';
 
 export const mockAuthStore = (value: Identity | null = mockIdentity) =>
-	vi.spyOn(authStore, 'authIdentity', 'get').mockImplementation(() => readable(value));
-
-export const mockAuthSignedIn = (value = true) => {
-	vi.spyOn(authStore, 'authSignedIn', 'get').mockImplementation(() => readable(value));
-	vi.spyOn(authStore, 'authNotSignedIn', 'get').mockImplementation(() => readable(!value));
-};
+    vi.spyOn(authStore, 'authIdentity', 'get').mockImplementation(() => readable(value));
